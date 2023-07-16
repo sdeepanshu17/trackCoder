200999b8011c4e941316dfec0d5fbee12cbb13b4
Deepanshu Singh <58651615+sdeepanshu17@users.noreply.github.com>
2023-07-16 10:56:16 +0530

    Update backend server to azure

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://codtrackerbackend.onrender.com";
+export const BASE_URL = "https://trackcoder.azurewebsites.net";
+// export const BASE_URL = "https://codtrackerbackend.onrender.com";
 // export const BASE_URL = "https://localhost:5001";

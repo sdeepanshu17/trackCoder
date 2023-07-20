7d8b94476a784e83e6132716ce385f96e6801045
Deepanshu Singh <deepanshu17092003@gmail.com>
2023-07-20 19:52:18 +0530

    Fix backend url

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,3 +1,3 @@
-// export const BASE_URL = "https://trackcoder.azurewebsites.net";
+export const BASE_URL = "https://trackcoder.azurewebsites.net";
 // export const BASE_URL = "https://codtrackerbackend.onrender.com";
-export const BASE_URL = "http://localhost:5001";
+// export const BASE_URL = "http://localhost:5001";
